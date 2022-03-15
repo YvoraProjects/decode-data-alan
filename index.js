811a9feb1a76f8467e7c70fcb9973952250d4f4a
Daniel <dferrandizmont@gmail.com>
2022-03-15 21:33:32 +0100

    Testing decode

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 var cors = require('cors');
+const fetch = require('node-fetch');
 const ethers = require("ethers");
 const pcsAbi = new ethers.utils.Interface(require("./abi.json"));
 const bodyParser = require("body-parser");
 const abiDecoder = require('abi-decoder');
-const { decodeLogs } = require('abi-decoder');
 
 app.set('port', process.env.PORT || 3001);
 app.set('json spaces', 2);
@@ -16,75 +16,94 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.post('/txs', cors(), async function (req, res) {
   res.contentType('application/json');
 
-  // https://bscscan.com/tx/0x15841cb11a8e62f691c7a49b0bde85b22ecff3281745cc35c641b19cb8493a13
-
   provider = new ethers.providers.WebSocketProvider('ws://localhost:8546');
   wallet = new ethers.Wallet('0xf0e72b4e27ea3f7542c8d1c3b1420865b7066f1767e20181fa513b47c625d1c0');
   account = wallet.connect(provider);
-
-  let abi = req.body.abi ? new ethers.utils.Interface(req.body.abi) : pcsAbi;
   let results = [];
-  abiDecoder.addABI(req.body.abi ? JSON.parse(req.body.abi) : require("./test-abi.json"));
 
-  if (!req.body.hashes) {
-    res.send(JSON.stringify(results));
-    return;
+  console.log(`---------------------------------------`);
+  console.log(`New petition ${new Date().toISOString()}`);
+
+  if (!req.body.apikey || !req.body.address || !req.body.hashes || req.body.hashes.length <= 0) {
+    respuesta = {
+      error: true,
+      codigo: 501,
+      mensaje: 'Algún dato vacío (Hash, Address, ApiKey)'
+    };
+
+    res.send(respuesta);
+    console.log(`Algun dato vacío`);
+    console.log(`---------------------------------------`);
   }
+
+  const apikey = req.body.apikey;
+  const address = req.body.address;
+  const abiResponse = await fetch(`https://api.bscscan.com/api?module=contract&action=getabi&address=${address}&apikey=${apikey}`);
+  const data = await abiResponse.json();
+  const abi = data?.result ? new ethers.utils.Interface(data.result) : pcsAbi;
+
+  try {
+    abiDecoder.addABI(data?.result ? JSON.parse(data.result) : require("./test-abi.json"));
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log(`Hashes: ${req.body.hashes.length}`);
+  console.log(`ApiKey: ${req.body.apikey}`);
+  console.log(`Address: ${req.body.address}`);
+  console.log(`---------------------------------------`);
+
+  var startTime = performance.now()
 
   for (let hash of req.body.hashes) {
 
-    await provider.getTransaction(hash)
-      .then(async (tx) => {
-        if (tx && tx.to) {
-          let decodedInput;
+    await provider.getTransaction(hash).then(async (tx) => {
+      if (tx && tx.to) {
+        let decodedInput;
 
+        try {
+          decodedInput = abi.parseTransaction({ data: tx.data, value: tx.value });
+        } catch (error) {
           try {
-            decodedInput = abi.parseTransaction({
-              data: tx.data,
-              value: tx.value,
-            });
+            decodedInput = pcsAbi.parseTransaction({ data: tx.data, value: tx.value });
           } catch (error) {
-            // console.log(error);
-            try {
-              decodedInput = pcsAbi.parseTransaction({
-                data: tx.data,
-                value: tx.value,
-              });
-            } catch (error) {
-              //console.log(error);
-            }
+            //console.log(error);
           }
+        }
 
-          const decodedLogsArray = [];
-          await provider.getTransactionReceipt(hash)
-            .then(async (tx) => {
+        const decodedLogsArray = [];
+        await provider.getTransactionReceipt(hash).then(async (tx) => {
+          try {
+            const decodedLogs = abiDecoder.decodeLogs(tx.logs);
+            if (decodedLogs && decodedLogs.length > 0) {
+              for (let log of decodedLogs) {
+                const newLog = Object.assign({}, log);
+                const eventArray = [];
 
-              try {
-                const decodedLogs = abiDecoder.decodeLogs(tx.logs);
-
-                for (let log of decodedLogs) {
-                  const newLog = Object.assign({}, log);
-                  const eventArray = [];
-
+                if (log && log.events && log.events.length > 0) {
                   for (let event of log.events) {
                     eventArray.push(event);
                   }
-
-                  newLog.events = eventArray;
-                  decodedLogsArray.push(newLog);
                 }
-              } catch (error) {
-                // console.log(error);
+
+                newLog.events = eventArray;
+                decodedLogsArray.push(newLog);
               }
-            });
+            }
+          } catch (error) {
+            // console.log(error);
+          }
+        })
 
-          results.push({ decodedInput, decodedLogsArray });
-        }
-      })
-      .catch((error) => { console.log(error) });
-
-
+        results.push({ decodedInput, decodedLogsArray });
+      }
+    })
   }
+
+  var endTime = performance.now()
+  console.log(`---------------------------------------`);
+  console.log(`Execution time ${((endTime - startTime) / 1000).toFixed(2)} sec.`);
+  console.log(`---------------------------------------`);
 
   res.send(JSON.stringify(results));
 });
